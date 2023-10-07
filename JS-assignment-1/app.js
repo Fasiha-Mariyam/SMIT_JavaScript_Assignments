dae804244196f3a28a9e51c978c985dfa648be23
Fasiha-Mariyam <125477695+Fasiha-Mariyam@users.noreply.github.com>
2023-10-07 09:53:15 +0500

    Add files via upload

---
+++ JS-assignment-1/app.js
@@ -0,0 +1,59 @@
+// Practice exercise 1.1
+// 1. Open the browser console, type 4 + 10, and press Enter. What do you see as
+// the response?
+// answer is 14
+
+// Use the console.log() syntax, placing a value within the rounded brackets.
+// Try entering your name with quotes around it (this is to indicate the fact that
+// it's a text string—we'll get to this in the next chapter).
+
+// Practice exercise 1.2
+// 1.Open your code editor and create an HTML file.
+// 2. Within your HTML file, set up the HTML tags, doctype, HTML, head, and
+// body, and then proceed and add the script tags.
+// 3. Place some JavaScript code within the script tags. You can use console.
+// log("hello world!").
+
+// Practice exercise 1.3
+// Create a separate file called app with the extension .js.
+// 2. Within the .js file, add some JavaScript code.
+// 3. Link to the separate .js file within the HTML file you created in Practice
+// exercise 1.2.
+// 4. Open the HTML file within your browser and check to see whether the
+// JavaScript code ran properly.
+console.log("hello world")
+
+// Practice exercise 1.4
+// 1. Add a new statement to your JavaScript code by setting a variable value.
+// Since we will cover this in the next chapter, you can use the following line:
+// let a = 10;
+// 2. Add a comment at the end of the statement indicating that you set a value of
+// 10.
+// 3. Print the value using console.log(). Add a comment explaining what this
+// will do.
+// 4. At the end of your JavaScript code, use a multiple-line comment. In a real
+// production script, you might use this space to add a brief outline of the
+// purpose of the file.
+let a=10; //value in variable a is 10
+console.log(a) //here it will console value of a
+
+console.log(Math.floor(Math.random() * 100));
+// Creating an HTML file and a linked JavaScript
+// file
+// Create an HTML file and create a separate JavaScript file. Then, connect to the
+// JavaScript file from the HTML file.
+
+// 1. In the JavaScript file, output your name into the console and add a multiple-
+// line comment to your code.
+// console.log("fasiha")
+// 2. Try commenting out the console message in your JavaScript file so that
+// nothing shows in the console.
+
+// Self-check quiz
+// 1. What is the HTML syntax to add an external JavaScript file?
+{/* <script src="./app.js"></script>  */}
+// 2. Can you run JavaScript in a file with a JS extension in your browser?
+// 3. How do you write a multiple-line comment in JavaScript?
+// 4. What is the best way to remove a line of code from running that you might
+// want to keep as you debug?
+//do comment
